8de3cd3f2dc538e6573b73da11904f73beb007a5
developerdesigner18 <developer.designer18@gmail.com>
2021-08-10 15:16:57 +0530

    added favoritedQuotes

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,28 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+  console.log(data.email);
+  //   if (context.auth.token.admin !== true) {
+  //     return { error: "Only admins can add other admins!!!" };
+  //   }
+
+  return admin
+    .auth()
+    .getUserByEmail(data.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, {
+        admin: true,
+      });
+    })
+    .then(() => {
+      return {
+        message: `Success! ${data.email} has been made an admin!`,
+      };
+    })
+    .catch((error) => {
+      return error;
+    });
+});
